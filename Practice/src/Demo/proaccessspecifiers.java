437100ef0aa208a7af2f68d5c41655dce715518f
Satish Das <satish.280484@gmail.com>
2021-11-19 00:38:31 -0600

    Access Modifiers

---
+++ Practice/src/Demo/proaccessspecifiers.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class proaccessspecifiers {
+	//protected void display() 
+	public void display()
+    { 
+        //System.out.println("This is protected access specifier"); 
+        System.out.println("This is Public access specifier");
+    } 
+}
---
+++ Practice/src/Oops/accessSpecifiers4.java
@@ -0,0 +1,13 @@
+package Oops;
+
+import Demo.*;
+
+public class accessSpecifiers4 {
+
+	public static void main(String[] args) {
+		proaccessspecifiers obj = new proaccessspecifiers();   
+	    obj.display();  
+	}
+
+}
+
